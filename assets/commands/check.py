4567bf56a4cbbc53ff0af928dd0705903295af44
Richard Godden <goddenrich@gmail.com>
2018-12-30 15:18:50 +0000

    moved methods out of phabricator class to allow for mocking in testing

--- assets/commands/check.py
+++ assets/commands/check.py
@@ -1,67 +1,58 @@
 from phabricator import Phabricator
-import sys
+import phabricator
 import json
 from common import Source, Version, Diff, Rev, Target, Build, Buildable
 from common import get_version_from_payload
 
 
-class Phabricator(Phabricator):
-    def __init__(self, conduit_url, conduit_token):
-        self.__init__(host=conduit_uri, token=conduit_token)
-        self.update_interfaces()
+# TODO(goddenrich) make this work if over max number returned by conduit
+def get_targets_since(phab, target_id):
+    if target_id:
+        search = phab.harbourmaster.target.search(order=['-id'], after=int(target_id)-1).get('data')
+    else:
+        search = phab.harbourmaster.target.search(limit=1).get('data')
+    return [Target(target_data) for target_data in search]
 
-    def get_latest_target(self):
-        return Target(self.harbourmaster.target.search(limit=1).get('data'))
+def get_build_from_PHID(phab, phid):
+    return Build(phab.harbourmaster.build.search(constraints={'phids': [phid]}).get('data')[0])
 
-    # TODO(goddenrich) make this work if over max number returned by conduit
-    def get_targets_since(self, target_id):
-        return [Target(target_data) for target_data in self.harbourmaster.target.search(order=['-id'], after=int(target_id)-1).get('data')]
+def get_buildable_from_PHID(phab, phid):
+    return Buildable(phab.harbourmaster.buildable.search(constraints={'phids': [phid]}).get('data')[0])
 
-    def get_build_from_PHID(self, phid):
-        return Build(self.harbourmaster.build.search(constraints={'phids': [phid]}).get('data')[0])
+def get_diff_from_PHID(phab, phid):
+    return Diff(phab.differential.diff.search(constraints={'phids': [phid]}).get('data')[0])
 
-    def get_buildable_from_PHID(self, phid):
-        return Buildable(self.harbourmaster.buildable.search(constraints={'phids': [phid]}).get('data')[0])
+def get_rev_from_PHID(phab, phid):
+    return Rev(phab.differential.revision.search(constraints={'phids': [phid]}).get('data')[0])
 
-    def get_diff_from_PHID(self, phid):
-        return Diff(self.differential.diff.search(constraints={'phids': [phid]}).get('data')[0])
+def get_build_from_target(phab, target):
+    return get_build_from_PHID(phab, target.buildPHID)
 
-    def get_rev_from_PHID(self, phid):
-        return Rev(self.differential.revision.search(constraints={'phids': [phid]}).get('data')[0])
+def get_buildable_from_build(phab, build):
+    return get_buildable_from_PHID(phab, build.buildablePHID)
 
-    def get_build_from_target(self, target):
-        return self.get_build_from_PHID(target.buildPHID)
+def get_diff_from_buildable(phab, buildable):
+    return get_diff_from_PHID(phab, buildable.objectPHID)
 
-    def get_buildable_from_build(self, build):
-        return self.get_buildable_from_PHID(build.buildablePHID)
+def get_rev_from_buildable(phab, buildable):
+    return get_rev_from_PHID(phab, buildable.containerPHID)
 
-    def get_diff_from_buildable(self, buildable):
-        return self.get_diff_from_PHID(buildable.objectPHID)
-
-    def get_rev_from_buildable(self, buildable):
-        return self.get_rev_from_PHID(buildable.containerPHID)
-
-    def get_diff_rev_from_target(self, target):
-        build = self.get_build_from_target(target)
-        buildable = self.get_buildable_from_build(build)
-        return target, self.get_diff_from_buildable(buildable), self.get_rev_from_buildable(buildable)
-
-def version_from_target_diff_rev(target, diff, rev):
+def version_from_target(phab, target):
+    build = get_build_from_target(phab, target)
+    buildable = get_buildable_from_build(phab, build)
+    diff = get_diff_from_buildable(phab, buildable)
+    rev = get_rev_from_buildable(phab, buildable)
     return Version(target.id, diff.id, diff.branch, rev.id)
 
-def get_new_versions(payload, phab):
+def get_new_versions(phab, payload):
     last_version = get_version_from_payload(payload)
-    if last_version is None:
-        new_targets = phab.get_latest_target()
-    else:
-        new_targets = phab.get_targets_since(last_version.target_id)
-    return [version_from_target_diff_rev(get_target_diff_rev_from_target(target)) for target in new_targets]
-
-
+    new_targets = get_targets_since(phab, last_version.target)
+    return [version_from_target(phab, target) for target in new_targets]
 
 if __name__ == "__main__":
     payload = json.loads(input())
     source = Source(payload)
-    phab = Phabricator(conduit_uri=source.conduit_uri, conduit_token = source.conduit_token)
-    new_versions = get_new_versions(payload, phab)
+    phab = Phabricator(host=source.conduit_uri, token=source.conduit_token)
+    phab.update_interfaces()
+    new_versions = get_new_versions(phab, payload)
     print(json.dumps(new_versions))
