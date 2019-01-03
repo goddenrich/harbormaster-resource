456170a4aa502f886405b457cca21f7a2ed17fd7
Richard Godden <goddenrich@gmail.com>
2019-01-03 19:01:40 +0000

    using configparser to get targetPHID from git config

--- assets/commands/out.py
+++ assets/commands/out.py
@@ -1,8 +1,11 @@
 from phabricator import Phabricator
+import configparser
 import phabricator
 import json
 from common import Source, Version, Diff, Rev, Target, Build, Buildable
 from common import get_version_from_payload, versions_to_json
+
+GIT_CONFIG_FILE = '.gitconfig'
 
 def get_build_status_from_payload(payload):
     params = payload.get('params', {}) or {}
@@ -11,8 +14,10 @@ def get_build_status_from_payload(payload):
 def update_phabricator_with_build_status(phab, targetPHID, build_status):
     phab.harbormaster.sendmessage(buildTargetPHID=targetPHID, type = build_status)
 
-def get_target_phid_from_git_config():
-    pass
+def get_target_phid(git_config):
+    config = configparser.ConfigParser()
+    config.read(git_config)
+    return config.get('phabricator', option='targetPHID')
 
 if __name__ == "__main__":
     payload = json.loads(input())
@@ -20,7 +25,7 @@ if __name__ == "__main__":
     phab = Phabricator(host=source.conduit_uri, token=source.conduit_token)
     phab.update_interfaces()
     build_status = get_build_status_from_payload(payload)
-    targetPHID = get_target_phid_from_git_config()
+    targetPHID = get_target_phid(GIT_CONFIG_FILE)
     update_phabricator_with_build_status(phab, build_status)
     print(json.dumps(versions_to_json(version)))
 
